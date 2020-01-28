036baa6d7ba7f4561e09a7e1e75fd3262a5ea29d
Soichiro-Yoshimura <yoshimura@soichiro.org>
2020-01-28 18:38:54 +0900

    mloginの実装完了

--- src/meritum.ts
+++ src/meritum.ts
@@ -1,15 +1,24 @@
 // Description:
 //   毎日ログインボーナスでもらった「めりたん」というポイントを使って遊ぶSlack用チャットボットゲーム
 
-import { Robot, Response } from 'hubot';
-import { Sequelize } from 'sequelize';
+import { Robot, Response, User } from 'hubot';
+import { Sequelize, Op } from 'sequelize';
+import moment from 'moment';
 
 import { database } from './models/sequelizeLoader';
 
 import { Account } from './models/accounts';
 import { LoginBonus } from './models/loginBonuses';
 
-import { Slack } from './slack'
+import { Slack } from './slack';
+
+/**
+ * ログインボーナス受領日を取得する、午前7時に変わるため、7時間前の時刻を返す
+ * @returns {Date} 7時間前の時刻
+ */
+function getReceiptToday(): Date {
+  return new Date(Date.now() - 1000 * 60 * 60 * 7);
+}
 
 // DB同期
 (async () => {
@@ -28,18 +37,18 @@ module.exports = (robot: Robot<any>) => {
   robot.hear(/^mhelp>$/i, (res: Response<Robot<any>>) => {
     res.send(
       'プロジェクトmeritumとは、めりたんを集めるプロジェクト。' +
-      '毎日のログインボーナスを集めて、ガチャを回し、称号を集めよう！' +
-      '他人に迷惑をかけたりしないように！めりたんが消滅します！' +
-      'めりたんbotをランキング100以下にしたら勝利！\n' +
-      '■コマンド説明\n' +
-      '`mhelp>` : めりたんbotの使い方を表示。\n' +
-      '`mlogin>` : ログインボーナスの100めりたんをゲット。毎朝7時にリセット。\n' +
-      '`mjanken> (1-10) (グー|チョキ|パー)` : めりたんbotと数値で指定しためりたんを賭けてジャンケン。\n' +
-      '`mgacha>` : 80めりたんでガチャを回して称号をゲット。\n' +
-      '`mself>` : 自分のめりたん、称号数、全称号、順位を表示。\n' +
-      '`mranking>` : 称号数、次にめりたんで決まるランキングを表示。\n' +
-      '`mrank> (@ユーザー名)` : 指定したユーザーのめりたん、称号数、全称号、順位を表示。\n' +
-      '`msend> (数値) (@ユーザー名)` : 指定したユーザーに数値で指定しためりたんを送る'
+        '毎日のログインボーナスを集めて、ガチャを回し、称号を集めよう！' +
+        '他人に迷惑をかけたりしないように！めりたんが消滅します！' +
+        'めりたんbotをランキング100以下にしたら勝利！\n' +
+        '■コマンド説明\n' +
+        '`mhelp>` : めりたんbotの使い方を表示。\n' +
+        '`mlogin>` : ログインボーナスの100めりたんをゲット。毎朝7時にリセット。\n' +
+        '`mjanken> (1-10) (グー|チョキ|パー)` : めりたんbotと数値で指定しためりたんを賭けてジャンケン。\n' +
+        '`mgacha>` : 80めりたんでガチャを回して称号をゲット。\n' +
+        '`mself>` : 自分のめりたん、称号数、全称号、順位を表示。\n' +
+        '`mranking>` : 称号数、次にめりたんで決まるランキングを表示。\n' +
+        '`mrank> (@ユーザー名)` : 指定したユーザーのめりたん、称号数、全称号、順位を表示。\n' +
+        '`msend> (数値) (@ユーザー名)` : 指定したユーザーに数値で指定しためりたんを送る'
     );
   });
 
@@ -54,10 +63,54 @@ module.exports = (robot: Robot<any>) => {
 
     const t = await database.transaction();
     try {
+      const receiptDate = getReceiptToday();
+      const countLoginBonus = await LoginBonus.count({
+        where: {
+          slackId: slackId,
+          receiptDate: {
+            [Op.eq]: receiptDate
+          }
+        }
+      });
 
-      const accont = await Account.findByPk(slackId);
+      if (countLoginBonus === 1) {
+        res.send(
+          `{@${slackId}>}さんは既に本日のログインボーナスを取得済みです。`
+        );
+        await t.commit();
+        return;
+      } else {
+        // アカウントがない場合には作り、100めりたん付与、ログインボーナス実績を追加
+        const oldAccount = await Account.findByPk(slackId);
+        if (!oldAccount) {
+          await Account.create({
+            slackId,
+            name,
+            realName,
+            displayName,
+            meritum: 100,
+            titles: '',
+            numOfTitles: 0
+          });
+        } else {
+          await Account.update(
+            {
+              meritum: oldAccount.meritum + 100
+            },
+            {
+              where: {
+                slackId: slackId
+              }
+            }
+          );
+        }
 
-      console.log(accont);
+        // ログインボーナス実績を作成
+        await LoginBonus.create({
+          slackId,
+          receiptDate
+        });
+      }
 
       await t.commit();
     } catch (e) {
@@ -66,5 +119,4 @@ module.exports = (robot: Robot<any>) => {
       await t.rollback();
     }
   });
-
 };
