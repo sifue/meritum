25f5848d6ba408753d43fd814c3262388370cb50
Soichiro Yoshimura <soichiro_yoshimura@nnn.ac.jp>
2020-09-17 13:13:24 +0900

    ジャンケンの上限を100に変更

--- scripts/constants.js
+++ scripts/constants.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 exports.LOGIN_BONUS_MERITUN = 100; // ログインボーナス
 exports.BEGGINERS_LUCK_FACTOR = 3; // ビギナーズラックの時のログインボーナス増加倍率
 exports.BOT_INITIAL_MERITUM = 20000; // ボットの初期めりたん
-exports.MAX_JANKEN_BET = 20; // 最大ベット
+exports.MAX_JANKEN_BET = 100; // 最大ベット
 exports.MAX_USER_JANKEN_BET = 100; // ユーザー同士ジャンケンの最大ベット
 exports.LIMIT_TIME_SEC_USER_JANKEN = 60; // ユーザー同士のジャンケンがキャンセルになる時間export
 exports.GACHA_MERITUM = 280; // ガチャ費用
